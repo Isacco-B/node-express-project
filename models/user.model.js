c7eceaa5bf23f025b8ad96d23533532f7b289665
Isacco-B <git@isaccobertoli.com>
2024-02-05 14:20:22 +0100

    create user model

---
+++ models/user.model.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+export default User;
